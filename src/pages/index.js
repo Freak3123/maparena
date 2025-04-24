87d2530a06dd88e33ef19ca9e71b70e63f4ae88b
freak3123 <snehashradha1234@gmaul.com>
2025-04-24 16:36:14 +0530

    refactor: remove unused imports from index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,6 @@
 import Hero from '@/components/Hero';
 import Navbar from '@/components/Navbar';
 import Quiz from '@/components/Quiz';
-import { Badge } from '@/components/ui/badge';
-import data from '@/data/data.json';
-
 
 export default function Home() {
   return (
